a8c35b639f03d4a92a81165cb08b1f6221bac20e
Michał Simonienko <michal.simonienko@protonmail.com>
2021-03-28 13:16:34 +0200

    Create Make and CarModel models

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+For the sake of time and simplicity I wasn't loading the full `Make` object from external API
+
+I choose name `CarModel` instead of `Model` to avoid name issues.
--- eycars/cars/models.py
+++ eycars/cars/models.py
@@ -3,5 +3,12 @@ from django.db import models
 from eycars.utils.models import BaseModel
 
 
-class Car(BaseModel):
-    pass
+class Make(BaseModel):
+    external_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=512)
+
+
+class CarModel(BaseModel):
+    external_id = models.IntegerField(unique=True)
+    make = models.ForeignKey(Make, on_delete=models.CASCADE, related_name="car_models")
+    name = models.CharField(max_length=1024)
